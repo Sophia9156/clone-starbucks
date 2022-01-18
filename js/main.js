d2a4d57142d77563f19625e4c191ffc166181938
Sophia9156 <suhyun9156@gmail.com>
2022-01-18 11:36:03 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
 
-// HEADER의 SEARCH 부분 애니메이션
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click',function(){
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus',function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-
-searchInputEl.addEventListener('blur',function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-});
-
-
 // HEADER의 badge 부분 애니메이션
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -162,8 +143,3 @@ spyEls.forEach(function(spyEl,k){
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-
-// 풋터 copyright 부분에 연도 계산
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
